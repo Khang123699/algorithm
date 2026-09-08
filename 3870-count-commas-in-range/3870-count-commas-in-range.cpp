90d67232430532cae72faa735d848ce62e494def
Khang Nguyen Nhat <khangnbk2004@gmail.com>
2026-09-08 19:01:00 +0700

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int totalCommas = 0;
+        long long currentThreshold = 1000;
+        while (n >= currentThreshold) {
+            totalCommas += (n - currentThreshold + 1);
+            currentThreshold *= 1000;
+        }
+        return totalCommas;
+    }
+};
